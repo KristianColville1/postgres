97cdcff4a18e9b5b6ed26136c0d657837cecddb9
KristianColville1 <kristiancolville1@gmail.com>
2022-06-22 15:26:42 +0100

    Perform all queries using sqlalchemy

--- sql-expression.py
+++ sql-expression.py
@@ -17,7 +17,7 @@ artist_table = Table(
 # create variable for "Album" table
 album_table = Table(
     "Album", meta,
-    Column("Album", Integer, primary_key=True),
+    Column("AlbumId", Integer, primary_key=True),
     Column("Title", String),
     Column("ArtistId", Integer, ForeignKey("artist_table.ArtistId"))
 )
@@ -28,7 +28,7 @@ track_table = Table(
     Column("TrackId", Integer, primary_key=True),
     Column("Name", String),
     Column("AlbumId", Integer, ForeignKey("album_table.AlbumId")),
-    Column("MediaType", Integer, primary_key=False),
+    Column("MediaTypeId", Integer, primary_key=False),
     Column("GenreId", Integer, primary_key=False),
     Column("Composer", String),
     Column("Milliseconds", Integer),
@@ -38,4 +38,25 @@ track_table = Table(
 
 # making the connection
 with db.connect() as connection:
-    
+
+    # Query 1 - select all records from the "Artist" table
+    # select_query = artist_table.select()
+
+    # Query 2 - select only the "Name" column from the "Artist" table
+    # select_query = artist_table.select().with_only_columns([artist_table.c.Name])
+
+    # Query 3 - select only 'Queen' from the "Artist" table
+    # select_query = artist_table.select().where(artist_table.c.Name == "Queen")
+
+    # Query 4 - select only by 'ArtistId' #51 from the "Artist" table
+    # select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
+
+    # Query 5 - select only the albums with 'ArtistId' #51 on the "Album" table
+    # select_query = album_table.select().where(album_table.c.ArtistId == 51)
+
+    # Query 6 - select all tracks where the composer is 'Queen' from the "Track" table
+    select_query = track_table.select().where(track_table.c.Composer == "Queen")
+
+    results = connection.execute(select_query)
+    for result in results:
+        print(result)
